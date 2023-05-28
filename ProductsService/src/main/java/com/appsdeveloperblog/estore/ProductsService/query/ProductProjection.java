f2d7c3abea6c3fb57dbc5d64b2dc9806c364e4fa
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-05-28 05:03:47 -0400

    r   - added product projection to product group processing group

--- ProductsService/src/main/java/com/appsdeveloperblog/estore/ProductsService/query/ProductProjection.java
+++ ProductsService/src/main/java/com/appsdeveloperblog/estore/ProductsService/query/ProductProjection.java
@@ -3,11 +3,13 @@ package com.appsdeveloperblog.estore.ProductsService.query;
 import com.appsdeveloperblog.estore.ProductsService.core.data.ProductEntity;
 import com.appsdeveloperblog.estore.ProductsService.core.data.ProductsRepository;
 import com.appsdeveloperblog.estore.ProductsService.core.events.ProductCreatedEvent;
+import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
+@ProcessingGroup("product-group")
 public class ProductProjection {
 
     private final ProductsRepository productsRepository;
