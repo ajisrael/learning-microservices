68eedb70759fc05d2905a389cffff395b91c7d64
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-05-27 21:52:20 -0400

    d   - removed noisy comments

--- ProductsService/src/main/java/com/appsdeveloperblog/estore/ProductsService/command/rest/ProductsCommandController.java
+++ ProductsService/src/main/java/com/appsdeveloperblog/estore/ProductsService/command/rest/ProductsCommandController.java
@@ -4,10 +4,7 @@ import com.appsdeveloperblog.estore.ProductsService.command.CreateProductCommand
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,19 +49,4 @@ public class ProductsCommandController {
 
         return returnValue;
     }
-
-//    @GetMapping
-//    public String getProduct() {
-//        return "HTTP GET Handled " + environment.getProperty("local.server.port");
-//    }
-//
-//    @PutMapping
-//    public String updateProduct() {
-//        return "HTTP PUT Handled";
-//    }
-//
-//    @DeleteMapping
-//    public String deleteProduct() {
-//        return "HTTP DELETE Handled";
-//    }
 }
