029505e1e25ef7334bad2253a70d34c55ac41bf7
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-05-27 20:40:51 -0400

    f   - created product projection

---
+++ ProductsService/src/main/java/com/appsdeveloperblog/estore/ProductsService/query/ProductProjection.java
@@ -0,0 +1,27 @@
+package com.appsdeveloperblog.estore.ProductsService.query;
+
+import com.appsdeveloperblog.estore.ProductsService.core.data.ProductEntity;
+import com.appsdeveloperblog.estore.ProductsService.core.data.ProductsRepository;
+import com.appsdeveloperblog.estore.ProductsService.core.events.ProductCreatedEvent;
+import org.axonframework.eventhandling.EventHandler;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductProjection {
+
+    private final ProductsRepository productsRepository;
+
+    public ProductProjection(ProductsRepository productsRepository) {
+        this.productsRepository = productsRepository;
+    }
+
+    @EventHandler
+    public void on(ProductCreatedEvent event) {
+
+        ProductEntity productEntity = new ProductEntity();
+        BeanUtils.copyProperties(event, productEntity);
+
+        productsRepository.save(productEntity);
+    }
+}
