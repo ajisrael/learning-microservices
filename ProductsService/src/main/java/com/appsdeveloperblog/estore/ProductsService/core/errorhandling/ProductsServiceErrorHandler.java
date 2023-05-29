4ee80ac068f224c567721869ddda62e03e94454f
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-05-28 20:09:42 -0400

    f   - added error handler for when exception occurs in command handler

--- ProductsService/src/main/java/com/appsdeveloperblog/estore/ProductsService/core/errorhandling/ProductsServiceErrorHandler.java
+++ ProductsService/src/main/java/com/appsdeveloperblog/estore/ProductsService/core/errorhandling/ProductsServiceErrorHandler.java
@@ -1,5 +1,6 @@
 package com.appsdeveloperblog.estore.ProductsService.core.errorhandling;
 
+import org.axonframework.commandhandling.CommandExecutionException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,18 @@ public class ProductsServiceErrorHandler {
 
     @ExceptionHandler(value = {IllegalStateException.class})
     public ResponseEntity<Object> handleIllegalStateException(IllegalStateException exception, WebRequest webRequest) {
+
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), exception.getMessage());
+
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = {CommandExecutionException.class})
+    public ResponseEntity<Object> handleCommandExecutionException(CommandExecutionException exception, WebRequest webRequest) {
+
+        // Note: this works because axon framework stages events to be processed. So even though the exception is being thrown
+        // after the event is applied to the aggregate lifecycle. The exception is being handled here before the event is dispatched
+        // and handled in the event handler on the aggregate
 
         ErrorMessage errorMessage = new ErrorMessage(new Date(), exception.getMessage());
 
