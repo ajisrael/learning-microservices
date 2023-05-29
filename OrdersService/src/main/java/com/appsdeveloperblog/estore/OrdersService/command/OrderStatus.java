c492bf7444338840f542a903f692bb27e77130b0
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-05-28 21:59:36 -0400

    f   - created rest controller, create order command, order created event, and order aggregate

---
+++ OrdersService/src/main/java/com/appsdeveloperblog/estore/OrdersService/command/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.appsdeveloperblog.estore.OrdersService.command;
+
+public enum OrderStatus {
+    CREATED, APPROVED, REJECTED
+}
---
+++ OrdersService/src/main/java/com/appsdeveloperblog/estore/OrdersService/command/rest/OrdersCommandController.java
@@ -0,0 +1,41 @@
+package com.appsdeveloperblog.estore.OrdersService.command.rest;
+
+import com.appsdeveloperblog.estore.OrdersService.command.CreateOrderCommand;
+import com.appsdeveloperblog.estore.OrdersService.command.OrderStatus;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/orders")
+public class OrdersCommandController {
+
+    private final CommandGateway commandGateway;
+
+    @Autowired
+    public OrdersCommandController(CommandGateway commandGateway) {
+        this.commandGateway = commandGateway;
+    }
+
+    @PostMapping
+    public String createProduct(@Valid @RequestBody CreateOrderRestModel createOrderRestModel) {
+
+        CreateOrderCommand createOrderCommand = CreateOrderCommand.builder()
+                .orderId(UUID.randomUUID().toString())
+                .userId("27b95829-4f3f-4ddf-8983-151ba010e35b")
+                .productId(createOrderRestModel.getProductId())
+                .quantity(createOrderRestModel.getQuantity())
+                .addressId(createOrderRestModel.getAddressId())
+                .orderStatus(OrderStatus.CREATED)
+                .build();
+
+        return commandGateway.sendAndWait(createOrderCommand);
+    }
+}
