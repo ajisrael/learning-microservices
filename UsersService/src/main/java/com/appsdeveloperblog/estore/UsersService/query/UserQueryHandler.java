5a952337d6d914701872f4869388c33f34490950
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-05-29 21:28:21 -0400

    f   - created users query handler in users service

---
+++ UsersService/src/main/java/com/appsdeveloperblog/estore/UsersService/query/UserQueryHandler.java
@@ -0,0 +1,29 @@
+package com.appsdeveloperblog.estore.UsersService.query;
+
+import com.appsdeveloperblog.estore.core.model.PaymentDetails;
+import com.appsdeveloperblog.estore.core.model.User;
+import com.appsdeveloperblog.estore.core.query.FetchUserPaymentDetailsQuery;
+import org.axonframework.queryhandling.QueryHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserQueryHandler {
+
+    @QueryHandler
+    public User findUserPaymentDetails(FetchUserPaymentDetailsQuery query) {
+        PaymentDetails paymentDetails = PaymentDetails.builder()
+                .cardNumber("123Card")
+                .cvv("123")
+                .name("SERGEY KARGOPOLOV")
+                .validUntilMonth(12)
+                .validUntilYear(2030)
+                .build();
+
+        return User.builder()
+                .firstName("Sergey")
+                .lastName("Kargopolov")
+                .userId(query.getUserId())
+                .paymentDetails(paymentDetails)
+                .build();
+    }
+}
