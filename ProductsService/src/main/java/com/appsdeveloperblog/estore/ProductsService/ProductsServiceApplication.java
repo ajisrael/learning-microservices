13e36195b4a0572c85bb3405fbff8f4566220c86
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-05-30 20:53:31 -0400

    f   - configured product snapshot trigger for products service

--- ProductsService/src/main/java/com/appsdeveloperblog/estore/ProductsService/ProductsServiceApplication.java
+++ ProductsService/src/main/java/com/appsdeveloperblog/estore/ProductsService/ProductsServiceApplication.java
@@ -4,11 +4,15 @@ import com.appsdeveloperblog.estore.ProductsService.command.interceptors.CreateP
 import com.appsdeveloperblog.estore.ProductsService.core.errorhandling.ProductServiceEventsErrorHandler;
 import org.axonframework.commandhandling.CommandBus;
 import org.axonframework.config.EventProcessingConfigurer;
+import org.axonframework.eventsourcing.EventCountSnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.SnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.Snapshotter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -29,6 +33,11 @@ public class ProductsServiceApplication {
 	public void configure(EventProcessingConfigurer configurer) {
 		configurer.registerListenerInvocationErrorHandler("product-group",
 				configuration -> new ProductServiceEventsErrorHandler());
+	}
+
+	@Bean(name = "productSnapshotTriggerDefinition")
+	public SnapshotTriggerDefinition productSnapshotTriggerDefinition(Snapshotter snapshotter) {
+		return new EventCountSnapshotTriggerDefinition(snapshotter, 3);
 	}
 
 }
