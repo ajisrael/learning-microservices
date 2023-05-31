d787824c79746a0de7ceec55f4f954993766a9fd
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-05-30 20:26:23 -0400

    f   - added subscription query result to orders controller on create order endpoint

--- OrdersService/src/main/java/com/appsdeveloperblog/estore/OrdersService/command/rest/OrdersCommandController.java
+++ OrdersService/src/main/java/com/appsdeveloperblog/estore/OrdersService/command/rest/OrdersCommandController.java
@@ -1,8 +1,13 @@
 package com.appsdeveloperblog.estore.OrdersService.command.rest;
 
 import com.appsdeveloperblog.estore.OrdersService.command.commands.CreateOrderCommand;
-import com.appsdeveloperblog.estore.OrdersService.core.model.OrderStatus;
+import com.appsdeveloperblog.estore.OrdersService.query.FindOrderQuery;
+import com.appsdeveloperblog.estore.core.model.OrderStatus;
+import com.appsdeveloperblog.estore.core.model.OrderSummary;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
+import org.axonframework.queryhandling.QueryGateway;
+import org.axonframework.queryhandling.SubscriptionQueryResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,24 +22,39 @@ import java.util.UUID;
 public class OrdersCommandController {
 
     private final CommandGateway commandGateway;
+    private final QueryGateway queryGateway;
 
     @Autowired
-    public OrdersCommandController(CommandGateway commandGateway) {
+    public OrdersCommandController(CommandGateway commandGateway, QueryGateway queryGateway) {
         this.commandGateway = commandGateway;
+        this.queryGateway = queryGateway;
     }
 
     @PostMapping
-    public String createProduct(@Valid @RequestBody CreateOrderRestModel createOrderRestModel) {
+    public OrderSummary createProduct(@Valid @RequestBody CreateOrderRestModel createOrderRestModel) {
+
+        String userId = "27b95829-4f3f-4ddf-8983-151ba010e35b";
+        String orderId = UUID.randomUUID().toString();
 
         CreateOrderCommand createOrderCommand = CreateOrderCommand.builder()
-                .orderId(UUID.randomUUID().toString())
-                .userId("27b95829-4f3f-4ddf-8983-151ba010e35b")
+                .orderId(orderId)
+                .userId(userId)
                 .productId(createOrderRestModel.getProductId())
                 .quantity(createOrderRestModel.getQuantity())
                 .addressId(createOrderRestModel.getAddressId())
                 .orderStatus(OrderStatus.CREATED)
                 .build();
 
-        return commandGateway.sendAndWait(createOrderCommand);
+        SubscriptionQueryResult<OrderSummary, OrderSummary> queryResult =
+                queryGateway.subscriptionQuery(new FindOrderQuery(orderId),
+                ResponseTypes.instanceOf(OrderSummary.class),
+                ResponseTypes.instanceOf(OrderSummary.class));
+
+        try {
+            commandGateway.sendAndWait(createOrderCommand);
+            return queryResult.updates().blockFirst();
+        } finally {
+            queryResult.close();
+        }
     }
 }
